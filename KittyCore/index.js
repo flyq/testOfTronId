f63431ecdfbfa2a36c25bdaaf100254fd07a5cfd
flyq <flyq951@gmail.com>
2019-01-01 01:53:06 +0800

    add test

---
+++ KittyCore/index.js
@@ -0,0 +1,67 @@
+// 环境检查 
+const waitForGlobal = async () => {
+  if (window.tronWeb) {
+    const tronWeb = window.tronWeb
+    const nodes = await tronWeb.isConnected()
+    const connected = !Object.entries(nodes).map(([key, value]) => {
+      if (!value) {
+        console.error(`Error: ${key} is not connected`)
+      }
+      return value
+    }).includes(false)
+    if (connected) {
+      const tronWeb = tronPay.tronWeb || tronWeb
+      tronApi.setTronWeb(tronWeb)
+    } else {
+      console.error('Error: TRON node is not connected')
+      console.error('wait for tronLink')
+      setTimeout(async () => {
+        await waitForGlobal()
+      }, 100)
+    }
+  } else {
+    console.error('wait for tronLink')
+    setTimeout(async () => {
+      await waitForGlobal()
+    }, 100)
+  }
+}
+
+waitForGlobal().then()
+
+
+const tronApi = {
+  tronWeb: false,
+  contract: false,
+  setTronWeb (tronWeb) {
+    this.tronWeb = tronWeb;
+    this.contract = tronWeb.contract(contracts.abi, '41b4f8413520f0a92b279c17fae1df92b1883791dc') // core
+  }
+}
+
+
+const send = document.querySelector('#send')
+const call = document.querySelector('#call')
+
+
+send.addEventListener('click', () => {
+  send.innerHTML = '发送中....'
+  tronApi.contract.exchange(1, 2, 'EOS', "KBY").send({
+    shouldPollResponse: true,
+    callValue: 0,
+  }).then(res => {
+    send.innerHTML = '已发送'
+    console.log('success', res)
+  }).catch(err => {
+    send.innerHTML = '已发送'
+    console.log('error', err)
+  })
+})
+
+call.addEventListener('click', () => {
+  tronApi.contract.getuint256(4).call().then(resp => {
+    console.log(resp, parseInt(resp.value._hex, 16), 'toNumber')
+  })
+})
+
+
